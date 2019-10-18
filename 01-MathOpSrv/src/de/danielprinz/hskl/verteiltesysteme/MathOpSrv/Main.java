3bac78d388a005f08f56edbd241044a328a80c57
physanus <github@danielprinz.de>
2019-10-18 13:14:41 +0200

    Initial commit

---
+++ 01-MathOpSrv/src/de/danielprinz/hskl/verteiltesysteme/MathOpSrv/Main.java
@@ -0,0 +1,10 @@
+package de.danielprinz.hskl.verteiltesysteme.MathOpSrv;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+}
